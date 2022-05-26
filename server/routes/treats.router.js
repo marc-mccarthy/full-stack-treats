33ba8fca40a0909fe6211b196afc6a4acf8ef305
theoryofmars <marstheory20@gmail.com>
2022-05-25 20:38:11 -0500

    Sending repo to remote.

---
+++ server/routes/treats.router.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const pool = require('../modules/pool');
+
+
+// GET /treats
+
+// POST /treats (req.body)
+
+// PUT /treats (req.query)
+
+// DELETE /treats (req.query)
+
+module.exports = router;
